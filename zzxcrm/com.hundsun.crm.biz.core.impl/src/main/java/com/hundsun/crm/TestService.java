ea1d9cddd15557965f294f5af6a7309cf0f71d40
zhouzhixing <zhou.zhi.xing@163.com>
2013-07-15 05:02:47 -0700

    新增了测试代码

---
+++ zzxcrm/com.hundsun.crm.biz.core.impl/src/main/java/com/hundsun/crm/TestService.java
@@ -0,0 +1,48 @@
+
+package com.hundsun.crm;
+
+import java.util.List;
+/**
+ * 
+ * TODO Comment of TestService
+ *
+ * @author zhouzx
+ * @version TestService.java 2013-7-14 下午09:05:26
+ *
+ */
+public interface TestService {
+	/**
+	 * 分页列表
+	 * @param obj
+	 * @param start 
+	 * @param limit
+	 * @return
+	 */
+	public List<TestObject> page(TestObject obj,int start,int limit);
+	
+	/**
+	 * 详情信息
+	 * @param obj
+	 * @return
+	 */
+	public TestObject getObject(TestObject obj);
+	
+	/**
+	 * 插入信息
+	 * @param obj
+	 */
+	public void insertObject(TestObject obj);
+	
+	/**
+	 * 更新信息
+	 * @param obj
+	 */
+	public void updateObject(TestObject obj);
+	
+	/**
+	 * 删除信息
+	 * @param id
+	 */
+	public void delObject(Integer id);
+
+}
