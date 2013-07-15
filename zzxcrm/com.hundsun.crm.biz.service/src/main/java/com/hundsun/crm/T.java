0e2d877dcb5cbae4ec810c601544aaa49150f144
zhouzhixing <zhou.zhi.xing@163.com>
2013-07-15 05:17:33 -0700

    测试代码

---
+++ zzxcrm/com.hundsun.crm.biz.service/src/main/java/com/hundsun/crm/T.java
@@ -0,0 +1,27 @@
+/**
+ * Project: com.hundsun.crm.biz.service
+ * 
+ * File Created at 2013-7-14
+ * $Id$
+ * 
+ * Copyright 2011 www.6677bank.com Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * 6677bank Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with 6677bank.com.
+ */
+package com.hundsun.crm;
+
+/**
+ * TODO Comment of T
+ *
+ * @author zhouzx
+ * @version T.java 2013-7-14 下午03:07:57
+ *
+ */
+public class T {
+
+}
