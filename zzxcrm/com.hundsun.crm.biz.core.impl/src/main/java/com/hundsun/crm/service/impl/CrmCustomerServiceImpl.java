db304b4c4c54e315abe04cf1a99196dc43abd408
leixl <leixl0324@163.com>
2013-07-15 21:00:28 +0800

    业务类搞一个

---
+++ zzxcrm/com.hundsun.crm.biz.core.impl/src/main/java/com/hundsun/crm/service/impl/CrmCustomerServiceImpl.java
@@ -0,0 +1,26 @@
+/**
+ * Project: com.hundsun.crm.biz.core.impl
+ * 
+ * File Created at 2013年7月15日
+ * $Id$
+ * 
+ * Copyright 2008 6677bank.com Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
+package com.hundsun.crm.service.impl;
+
+/**
+ *  
+ * @author leixl
+ * @email  leixl0324@163.com
+ * @date   2013年7月15日 下午8:55:37
+ * @version v1.0
+ */
+public class CrmCustomerServiceImpl {
+
+	
+}
