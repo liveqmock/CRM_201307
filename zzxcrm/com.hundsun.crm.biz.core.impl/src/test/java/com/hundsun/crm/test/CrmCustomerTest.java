35e057cb7489646ce8775d2143e87e107443e575
leixl <leixl0324@163.com>
2013-07-16 22:59:04 +0800

    修改下

--- zzxcrm/com.hundsun.crm.biz.core.impl/src/test/java/com/hundsun/crm/test/CrmCustomerTest.java
+++ zzxcrm/com.hundsun.crm.biz.core.impl/src/test/java/com/hundsun/crm/test/CrmCustomerTest.java
@@ -13,6 +13,22 @@
  */
 package com.hundsun.crm.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hundsun.beyond.common.vo.PageInfo;
+import com.hundsun.crm.bean.CrmCustomer;
+import com.hundsun.crm.service.CrmCustomerService;
+
 /**
  *  
  * @author leixl
@@ -20,7 +36,24 @@ package com.hundsun.crm.test;
  * @date   2013年7月16日 下午10:40:39
  * @version v1.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
 public class CrmCustomerTest {
+	
+	public final static Logger logger = LoggerFactory.getLogger(CrmCustomerTest.class);
 
+	@Autowired
+	private CrmCustomerService crmCustomerService;
+	
+	@BeforeClass
+	public static void init(){
+	}
+
+	@Test
+	public void testFindPage(){
+		Map<String, Object> paramMap =  new HashMap<String,Object>();
+		PageInfo<CrmCustomer> pageInfo = crmCustomerService.findPage(paramMap, 1, 15);
+		logger.info("result is "+pageInfo.getResult());
+	}
 	
 }
