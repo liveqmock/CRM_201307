58b1ced0aaf38979c80cb814eef4b5ee509b78a0
leixl <leixl0324@163.com>
2013-07-16 22:44:00 +0800

    配置文件

---
+++ zzxcrm/com.hundsun.crm.biz.core.impl/src/test/java/com/hundsun/crm/test/CrmCustomerTest.java
@@ -0,0 +1,26 @@
+/**
+ * Project: com.hundsun.crm.biz.core.impl
+ * 
+ * File Created at 2013年7月16日
+ * $Id$
+ * 
+ * Copyright 2008 6677bank.com Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ */
+package com.hundsun.crm.test;
+
+/**
+ *  
+ * @author leixl
+ * @email  leixl0324@163.com
+ * @date   2013年7月16日 下午10:40:39
+ * @version v1.0
+ */
+public class CrmCustomerTest {
+
+	
+}
