74a90e3fb5280cc1a82d3fc648a6516f453859e5
zhouzhixing <zhou.zhi.xing@163.com>
2013-07-19 20:55:33 -0700

    新增类

---
+++ zzxcrm/com.hundsun.crm.plugin.cep/src/main/java/com/hundsun/crm/cep/LinkManCepImpl.java
@@ -0,0 +1,27 @@
+/**
+ * Project: com.hundsun.crm.plugin.cep
+ * 
+ * File Created at 2013-7-20
+ * $Id$
+ * 
+ * Copyright 2011 www.6677bank.com Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * 6677bank Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with 6677bank.com.
+ */
+package com.hundsun.crm.cep;
+
+/**
+ * 联系人 LinkManCepImpl
+ *
+ * @author zhouzx
+ * @version LinkManCepImpl.java 2013-7-20 上午10:02:35
+ *
+ */
+public class LinkManCepImpl {
+
+}
