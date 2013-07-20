7200b60f304b701f544f15e5232b9bff3817abfb
zhouzhixing <zhou.zhi.xing@163.com>
2013-07-20 06:57:26 -0700

    新增修改代码

--- zzxcrm/com.hundsun.crm.plugin.cep/src/main/java/com/hundsun/crm/cep/CustomerCepImpl.java
+++ zzxcrm/com.hundsun.crm.plugin.cep/src/main/java/com/hundsun/crm/cep/CustomerCepImpl.java
@@ -15,6 +15,7 @@
  */
 package com.hundsun.crm.cep;
 
+import java.util.Date;
 import java.util.List;
 
 import com.beyond.crm.bean.CrmCustomer;
@@ -24,6 +25,7 @@ import com.hundsun.crm.common.util.WrapperUtil;
 import com.hundsun.crm.wrapper.anotation.JresService;
 import com.hundsun.jres.interfaces.cep.context.IEventContext;
 import com.hundsun.jres.interfaces.share.dataset.IDataset;
+import com.hundsun.jres.interfaces.share.dataset.IDatasetMetaData;
 import com.hundsun.jres.interfaces.share.dataset.IDatasets;
 
 /**
@@ -50,26 +52,71 @@ public class CustomerCepImpl {
 		
 		return WrapperUtil.createListResult(list, CrmCustomer.class, list==null?0:list.size());
 	}
-/*	@JresService(alias = "customer.service.cust.findCustomerDetail", desc = "客户详情信息", value = "customer.service.cust.findCustomerDetail")
+	@JresService(alias = "customer.serviceInterface.cust.findCustomerDetail", desc = "客户详情信息", value = "customer.service.cust.findCustomerDetail")
 	public IDatasets findCustomerDetail(IEventContext context,
 			IDataset request)  {
 		CrmCustomer obj = getObject("周星驰","男","星爷",30);
 		
 		return WrapperUtil.createObjectResult(obj, CrmCustomer.class);
 	}
-	@JresService(alias = "customer.service.cust.delCustomer", desc = "删除客户详情信息", value = "customer.service.cust.delCustomer")
-	public IDatasets delCustomer(IEventContext context,
+	@JresService(alias = "customer.serviceInterface.cust.insertCustomer", desc = "新增客户详情信息", value = "customer.serviceInterface.cust.insertCustomer")
+	public IDatasets insertCustomer(IEventContext context,
 			IDataset request)  {
 		
+		String custNo = request.getString("custNo");             
+		String custName = request.getString("custName");           
+		String fullPinyinName = request.getString("fullPinyinName");     
+		String simplePinyinName = request.getString("simplePinyinName");   
+		String stockCode = request.getString("stockCode");          
+		String invoiceAddress = request.getString("invoiceAddress");     
+		String custHot = request.getString("custHot");           
+		String custSource = request.getString("custSource");         
+		String custType = request.getString("custType");           
+		String employeeTotal = request.getString("employeeTotal");     
+		String custRegion = request.getString("custRegion");         
+		String custLevel = request.getString("custLevel");          
+		String custSatisfy = request.getString("custSatisfy");        
+		String custCredit = request.getString("custCredit");         
+		String country = request.getString("country");            
+		String province = request.getString("province");           
+		String city = request.getString("city");               
+		String custAddr = request.getString("custAddr");           
+		String custZipCode = request.getString("custZipCode");        
+		String custTel = request.getString("custTel");            
+		String custFax = request.getString("custFax");            
+		String custWebsite = request.getString("custWebsite");        
+		String custLicenceNo = request.getString("custLicenceNo");      
+		String custChieftain = request.getString("custChieftain");      
+		String custBankroll = request.getString("custBankroll");         
+		String custTurnover = request.getString("custTurnover");         
+		String custBank = request.getString("custBank");           
+		String custBankAccount = request.getString("custBankAccount");    
+		String custLocalTaxNo = request.getString("custLocalTaxNo");     
+		String custNationalTaxNo = request.getString("custNationalTaxNo");  
+		String custStatus = request.getString("custStatus");
+		CrmCustomer obj = new CrmCustomer();
+		obj.setCity(city);
+		obj.setCountry(country);
+		obj.setCreateDate(new Date());
+		obj.setCreateUserId("admin");
+		obj.setCustName(custName);
+		this.customerService.saveCustomer(obj);
 		return WrapperUtil.createSuccessResult();
 	}
-	@JresService(alias = "customer.service.cust.updateCustomer", desc = "修改客户详情信息", value = "customer.service.cust.updateCustomer")
+	@JresService(alias = "customer.serviceInterface.cust.updateCustomer", desc = "修改客户详情信息", value = "customer.service.cust.updateCustomer")
 	public IDatasets updateCustomer(IEventContext context,
 			IDataset request)  {
 		CrmCustomer obj = getObject("周星驰","男","星爷",30);
 		
 		return WrapperUtil.createSuccessResult();
-	}*/
+	}
+	@JresService(alias = "customer.serviceInterface.cust.delCustomer", desc = "删除客户详情信息", value = "customer.service.cust.delCustomer")
+	public IDatasets delCustomer(IEventContext context,
+			IDataset request)  {
+		
+		return WrapperUtil.createSuccessResult();
+	}
+
 	/**
 	 * @return
 	 */
